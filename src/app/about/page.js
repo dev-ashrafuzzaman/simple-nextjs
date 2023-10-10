e15e49c93a1c92d3916b382b70706d0c18b8590f
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-10-10 18:53:45 +0600

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+   
+    return (
+  
+        <div>
+           This is About Pages 
+        </div>
+    );
+};
+
+export default AboutPage;
