809ef9f9785ed701eca8a966bcbd982c410e603e
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-10-11 22:30:52 +0600

    [DONE Sigment ]

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const AboutPage = () => {
-   
+
     return (
-  
+
         <div>
-           This is About Pages 
+            This is About Pages
+
+            <div>
+                <h1 className='text-[80px] font-bold my-10 bg-[#3E7E97] text-white p-20'>
+                    Dactar Link
+                </h1>
+                <h1 className='text-[80px] font-bold my-10 bg-[#F25822] text-white p-20'>
+                    Dactar Link
+                </h1>
+                <h1 className='text-[80px] font-bold my-10 bg-[#075E54] text-white p-20'>
+                    Dactar Link
+                </h1>
+                <h1 className='text-[80px] font-bold my-10 bg-[#fde9e2c7]  text-[#F25822] p-20'>
+                    Dactar <span>Link</span>
+                </h1>
+                <div>
+                    <span className='bg-[#F25822] text-white text-[80px] font-bold my-10 px-6 rounded-2xl mx-4'>Dactar</span>
+                    <span className=' text-[80px] font-bold my-10  text-black '>Link</span>
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/blogs/[[...sigments]]/page.js
+++ src/app/blogs/[[...sigments]]/page.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const SingleBlogs = ({params}) => {
     console.log(params);
+    const [year , id] = params.sigments || [];
     return (
         <div>
-            My Silgle Blogs NO: {params.id}
+            My Silgle Blogs NO: Year {year || new Date().getFullYear() } for id: {id || 0}
         </div>
     );
 };
